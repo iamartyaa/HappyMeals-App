9d95061c4dbbb49fd72fb6496ef6bd4f50e2cc1a
Amartya Yadav <iamartyayadav@gmail.com>
2022-02-27 23:41:50 +0530

    Final Touch

--- lib/screens/favourites_screen.dart
+++ lib/screens/favourites_screen.dart
@@ -11,7 +11,7 @@ class FavouritesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if(favMeals.isEmpty){
-    return Center(
+    return const Center(
       child: Text('You Have no Favourites !'),
     );
     }
